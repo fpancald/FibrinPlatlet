b43b7288af3405f355f7d568ad23073250841ed7
fpancald <fpancald@nd.edu>
2018-11-06 14:28:03 -0500

    cleanup and creation of .gitignore file

--- objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := 
-
-LIBS := -lgsl -lgslcblas -lpugixml -L/afs/crc.nd.edu/user/s/sbritto2/FibrinPlatlet/pugixml/lib64
-ILIBS := -I/afs/crc.nd.edu/x86_64_linux/c/cuda/8.0/include/
