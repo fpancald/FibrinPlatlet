72782929a2eef456dabd4bf062e2cb963fb33621
fpancald <fpancald@nd.edu>
2019-03-21 14:30:59 -0400

    cleanup and new simulations

--- SeededNetwork/run.sh
+++ SeededNetwork/run.sh
@@ -1,14 +1,14 @@
 #!/bin/csh
 
-#$ -M 	@ucr.edu # Email address for job notification
+#$ -M 	francesp@ucr.edu # Email address for job notification
 #$ -m  abe		 # Send mail when job begins, ends and aborts
 #$ -q  gpu 	 # Specify queue
-#$ -l gpu_card=5
+#$ -l gpu_card=1
 #$ -pe smp 4         #specifies threads??? maybe
-#$ -N  pltdeg6	 # Specify job name
-#$ -t 1       #specify number of data input files
+#$ -N  pltNucleationRelease	 # Specify job name
+#$ -t 1-2:1       #specify number of data input files
 
-set data = ( data_Rec_0.025375_20_20_20_4deg6.xml )
+set data = ( data_Rec_0.0238_20_20_20_0.8.xml data_Rec_0.0238_20_20_20_0.7.xml )
 
 
 module purge
@@ -21,4 +21,4 @@ echo -n "This computer is called ";hostname
 echo -n "I am currently in the directory ";pwd
 
 
-./bend-model -eps=0.0001 -dt=0.001 $data[${SGE_TASK_ID}]
+./bend-model -eps=0.0001 -dt=0.00025 $data[${SGE_TASK_ID}]
